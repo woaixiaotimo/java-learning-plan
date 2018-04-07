9844e2aea69565a20d9f45df68e5bbed0edaafae
woaixiaotimo <1084716575@qq.com>
2018-04-08 06:38:13 +0800

    2

--- java核心技术学习整理-2/18.web基础/demoServlet/src/com/controller/controller.java
+++ java核心技术学习整理-2/18.web基础/demoServlet/src/com/controller/controller.java
@@ -9,8 +9,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@Service
+@Service  
 public class Controller {
+
 
     @ControllerUrl("/test")
     public void test(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
