ed187bd57c7a523bae0066f931976c190b9a9c65
woaixiaotimo <1084716575@qq.com>
2018-04-10 17:47:58 +0800

    1

--- java核心技术学习整理-2/18.web基础/demoServlet/src/test/Test.java
+++ java核心技术学习整理-2/18.web基础/demoServlet/src/test/Test.java
@@ -7,10 +7,27 @@ public class Test {
     public static void main(String[] args) throws UnsupportedEncodingException {
         String a = "https://bbs.csdn.net/topics/300154555.do";
         String[] b = a.split(".do");
-        for (int i = 0; i <b.length ; i++) {
+        for (int i = 0; i < b.length; i++) {
             System.out.println("b[i] = " + b[i]);
         }
 
     }
 
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
