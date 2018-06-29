7ecf6f0dc42bdb5662d2fa02c6a79458663e05bd
woaixiaotimo <1084716575@qq.com>
2018-06-29 18:03:03 +0800

    添加1-4

---
+++ my-demo/src/com/futureDemo2/Main.java
@@ -0,0 +1,27 @@
+package com.futureDemo2;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.*;
+
+public class Main {
+    public static class Task implements Callable<String> {
+        @Override
+        public String call() throws Exception {
+            String tid = String.valueOf(Thread.currentThread().getId());
+            System.out.printf("Thread#%s : in call\n", tid);
+            Thread.sleep(5000);
+            return tid;
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
+        List<Future<String>> results = new ArrayList<Future<String>>();
+        ExecutorService es = Executors.newCachedThreadPool();
+        for(int i=0; i<1;i++)
+            results.add(es.submit(new Task()));
+
+        for(Future<String> res : results)
+            System.out.println(res.get());
+    }
+}
