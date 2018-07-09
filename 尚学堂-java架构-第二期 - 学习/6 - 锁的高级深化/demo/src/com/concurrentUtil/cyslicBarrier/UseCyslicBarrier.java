29b5e3e2c2e3f90bab417be94365f7d0b05f87c9
woaixiaotimo <1084716575@qq.com>
2018-07-10 07:21:53 +0800

    123

---
+++ 尚学堂-java架构-第二期 - 学习/6 - 锁的高级深化/demo/src/com/concurrentUtil/cyslicBarrier/UseCyslicBarrier.java
@@ -0,0 +1,50 @@
+package com.concurrentUtil.cyslicBarrier;
+
+import java.util.Random;
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class UseCyslicBarrier {
+
+    static class Runner implements Runnable {
+
+
+        CyclicBarrier barrier;
+        String name;
+
+        public Runner(CyclicBarrier barrier, String name) {
+            this.barrier = barrier;
+            this.name = name;
+        }
+
+        @Override
+        public void run() {
+
+            try {
+                Thread.sleep(1000 * (new Random().nextInt(5)));
+                System.out.println(name + " 准备OK");
+                barrier.await();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (BrokenBarrierException e) {
+                e.printStackTrace();
+            }
+            System.out.println(name + " go");
+
+        }
+    }
+
+    public static void main(String[] args) {
+        CyclicBarrier barrier = new CyclicBarrier(3);
+        ExecutorService executorService = Executors.newFixedThreadPool(3);
+
+        executorService.submit(new Thread(new Runner(barrier, "1")));
+        executorService.submit(new Thread(new Runner(barrier, "2")));
+        executorService.submit(new Thread(new Runner(barrier, "3")));
+
+        executorService.shutdown();
+    }
+
+}
