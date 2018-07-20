ec4fed4ae3032876dc0095caad5272f9bef25102
woaixiaotimo <1084716575@qq.com>
2018-07-20 17:58:10 +0800

    第七天开始

---
+++ 尚学堂-java架构-第二期 - 学习/7 - 并发编程框架篇-Disruptor并发框架/demodisruptor/src/main/java/com/base/LongEventHandler.java
@@ -0,0 +1,16 @@
+package com.base;
+
+import com.lmax.disruptor.EventHandler;
+
+public class LongEventHandler implements EventHandler<LongEvent> {
+    public Long afterTime;
+
+    public void onEvent(LongEvent longEvent, long l, boolean b) throws Exception {
+//        if (longEvent.getValue() == 1048576L) {
+//            afterTime = System.currentTimeMillis();
+//            System.out.println("thisTime = " +(afterTime - LongEventMain.beforeTime));
+//        }
+
+        System.out.println(longEvent.getValue());
+    }
+}
