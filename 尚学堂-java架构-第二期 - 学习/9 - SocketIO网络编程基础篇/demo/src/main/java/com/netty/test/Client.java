2e76f23b0d87325342afd208035413a97218ec9c
woaixiaotimo <1084716575@qq.com>
2018-07-31 17:49:31 +0800

    1

--- 尚学堂-java架构-第二期 - 学习/9 - SocketIO网络编程基础篇/demo/src/main/java/com/netty/test/Client.java
+++ 尚学堂-java架构-第二期 - 学习/9 - SocketIO网络编程基础篇/demo/src/main/java/com/netty/test/Client.java
@@ -1,4 +1,38 @@
 package com.netty.test;
 
+import io.netty.bootstrap.Bootstrap;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
 public class Client {
+    public static void main(String[] args) throws InterruptedException {
+
+        EventLoopGroup group = new NioEventLoopGroup();
+
+
+        Bootstrap b = new Bootstrap();
+        b.group(group)
+                .channel(NioSocketChannel.class)
+                .handler(new ChannelInitializer<SocketChannel>() {
+                    protected void initChannel(SocketChannel socketChannel) throws Exception {
+                        socketChannel.pipeline().addLast(new ClientHandler());
+                    }
+                });
+        ChannelFuture cf1 = b.connect("127.0.0.1", 8765).sync();
+
+        //buf
+        cf1.channel().write(Unpooled.copiedBuffer("111".getBytes()));
+        cf1.channel().flush();
+
+
+        cf1.channel().closeFuture().sync();
+        group.shutdownGracefully();
+
+
+    }
 }
