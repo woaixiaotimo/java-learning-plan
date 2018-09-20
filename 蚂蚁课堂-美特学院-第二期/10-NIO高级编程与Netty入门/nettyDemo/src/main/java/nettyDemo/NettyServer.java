f509aa3f673913a627e1c09f15d31d4a12aefb51
woaixiaotimo <1084716575@qq.com>
2018-09-20 17:49:27 +0800

    1

---
+++ 蚂蚁课堂-美特学院-第二期/10-NIO高级编程与Netty入门/nettyDemo/src/main/java/nettyDemo/NettyServer.java
@@ -0,0 +1,38 @@
+package nettyDemo;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+/**
+ * 服务端
+ * Netty5
+ */
+public class NettyServer {
+
+    private static final int port = 6789; //设置服务端端口
+    private static  EventLoopGroup group = new NioEventLoopGroup();   // 通过nio方式来接收连接和处理连接
+    private static  ServerBootstrap serverBootstrap = new ServerBootstrap();
+
+    /**
+     * Netty创建全部都是实现自AbstractBootstrap。
+     * 客户端的是Bootstrap，服务端的则是    ServerBootstrap。
+     **/
+    public static void main(String[] args) throws InterruptedException {
+        try {
+
+            serverBootstrap.group(group);
+            //绑定通道类型
+            serverBootstrap.channel(NioServerSocketChannel.class);
+            serverBootstrap.childHandler(new NettyServerFilter()); //设置过滤器
+            // 服务器绑定端口监听
+            ChannelFuture f = serverBootstrap.bind(port).sync();
+            System.out.println("服务端启动成功...");
+            // 监听服务器关闭监听
+            f.channel().closeFuture().sync();
+        } finally {
+            group.shutdownGracefully(); ////关闭EventLoopGroup，释放掉所有资源包括创建的线程
+        }
+    }
+}
