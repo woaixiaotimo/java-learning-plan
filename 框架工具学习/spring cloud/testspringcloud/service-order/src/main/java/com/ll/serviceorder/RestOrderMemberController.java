edd4b3683dec43d498a34c624e064d4d3b6894dc
woaixiaotimo <1084716575@qq.com>
2018-12-09 12:33:24 +0800

    完成spring eureka 学习笔记

--- 框架工具学习/spring cloud/testspringcloud/service-order/src/main/java/com/ll/serviceorder/RestOrderMemberController.java
+++ 框架工具学习/spring cloud/testspringcloud/service-order/src/main/java/com/ll/serviceorder/RestOrderMemberController.java
@@ -1,5 +1,7 @@
 package com.ll.serviceorder;
 
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,9 +13,19 @@ public class RestOrderMemberController {
     @Autowired
     private RestTemplate restTemplate;
 
+
+    @HystrixCommand(fallbackMethod = "defaultStores")
     @RequestMapping("/restGetOrderMember")
     public String getOrderMember() {
         return "order - " + restTemplate.getForObject("http://service-member/getMember", String.class);
+    }
+
+
+    public String defaultStores() {
+
+//        System.out.println("parameters = " + parameters);
+//        return parameters;
+        return "error";
     }
 
 }
--- 框架工具学习/spring cloud/testspringcloud/service-order/src/main/java/com/ll/serviceorder/ServiceOrderApplication.java
+++ 框架工具学习/spring cloud/testspringcloud/service-order/src/main/java/com/ll/serviceorder/ServiceOrderApplication.java
@@ -2,9 +2,13 @@ package com.ll.serviceorder;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 @EnableEurekaClient
+//@EnableCircuitBreaker
+@EnableHystrix
 @SpringBootApplication
 public class ServiceOrderApplication {
 
